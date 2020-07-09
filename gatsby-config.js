c096e95324848685d30f222ab82828bf92c5b7a7
lulu70 <liorco70@yahoo.com>
2020-07-09 12:16:59 +0200

    added mobile statusbar component

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
+      },
+    },
     {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,39 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql } from "gatsby"
+import MobileStatusBar from "../components/MobileStatusBar"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Font test</h1>
-    <p>testing the font</p>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const salons = data.dataJson.salons
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <MobileStatusBar />
+      <ul>
+        {salons.map(salon => (
+          <li key={salon.id}>{salon.name}</li>
+        ))}
+      </ul>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    dataJson {
+      salons {
+        id
+        name
+        address
+        time
+        rating
+        ratingCount
+        price
+        timeSlot
+      }
+    }
+  }
+`
