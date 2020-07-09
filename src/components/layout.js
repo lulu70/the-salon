4d040bcdd86d4eb3a80ae41b632c0588e4e116cf
lulu70 <liorco70@yahoo.com>
2020-07-09 10:03:21 +0200

    design basic layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,46 +1,19 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
 import "./layout.css"
+import styled from "styled-components"
 
+const Container = styled.div`
+  margin: 2rem auto;
+  width: 375px;
+  height: 667px;
+  border: solid 1px;
+`
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <Container>
+      <main>{children}</main>
+    </Container>
   )
 }
 
